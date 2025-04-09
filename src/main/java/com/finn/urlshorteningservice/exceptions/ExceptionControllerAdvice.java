62673b345986376b74b76521a81839f2919a8313
Finn <finefinnfinning@gmail.com>
2025-04-09 15:25:04 +0700

    Add custom response message for url validation

--- src/main/java/com/finn/urlshorteningservice/exceptions/ExceptionControllerAdvice.java
+++ src/main/java/com/finn/urlshorteningservice/exceptions/ExceptionControllerAdvice.java
@@ -3,6 +3,7 @@ package com.finn.urlshorteningservice.exceptions;
 import com.finn.urlshorteningservice.models.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +14,15 @@ public class ExceptionControllerAdvice {
     public ResponseEntity<ErrorDetails> exceptionShortCodeNotFoundHandler() {
         ErrorDetails errorDetails = new ErrorDetails();
         errorDetails.setMessage("Shortcode not found");
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorDetails);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> exceptionMethodArgumentNotValidHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Missing or invalid url");
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(errorDetails);
